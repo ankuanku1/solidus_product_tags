01c5b4878a8cb61c9204c8057269425c8352a1cc
Anssi <anssisanteri.kuokkanen@gmail.com>
2025-07-15 11:25:15 +0900

    removed unneeded files and code as the gem is not using solidus_admin

--- lib/solidus_product_tags/engine.rb
+++ lib/solidus_product_tags/engine.rb
@@ -9,20 +9,5 @@ module SolidusProductTags
         ::Spree::Product.include SolidusProductTags::ProductTaggable
       end
     end
-
-    initializer "solidus_product_tags.decorate_solidus_admin_components" do
-      config.after_initialize do
-        ActiveSupport.on_load(:after_initialize) do
-          begin
-            require "solidus_product_tags/admin/product_form_component_decorator"
-            SolidusAdmin::Products::FormComponent.prepend(
-              SolidusProductTags::Admin::ProductFormComponentDecorator
-            )
-          rescue NameError => e
-            Rails.logger.warn("[solidus_product_tags] Could not decorate SolidusAdmin component: #{e.message}")
-          end
-        end
-      end
-    end
   end
 end
